3e0ea906afb336e3eede3e2eab4aee528703696d
Shrivu Shankar <shrivu1122@gmail.com>
2017-06-08 13:55:39 -0500

    Quicksort

---
+++ Algorithms/Sorting/Quicksort.py
@@ -0,0 +1,36 @@
+
+def partition(nums, low, high):
+
+    pivot = nums[high]
+    i = low - 1
+
+    for j in range(low, high):
+
+        if nums[j] <= pivot:
+
+            i += 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+    nums[i + 1], nums[high] = nums[high], nums[i + 1]
+
+    return i + 1
+
+def quick_sort(nums, low=None, high=None):
+
+    if low == None or high == None:
+
+        quick_sort(nums, 0, len(nums) - 1)
+
+    elif low < high:
+
+        index = partition(nums, low, high)
+
+        quick_sort(nums, low, index - 1)
+        quick_sort(nums, index + 1, high)
+
+nums = [1, 5, 2, 6, 10, 25, 15, 12, 8]
+
+print(nums)
+quick_sort(nums)
+print(nums)
